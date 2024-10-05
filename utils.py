9882a27785aebee24addf4051da06c0df395c3dc
VoidOG <VoidGod123@outlook.com>
2024-10-05 13:00:51 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+import os
+
+LOG_FILE = "Compress_logs.txt"
+
+def log_usage(message, bot=None):
+    """Logs bot usage to a file and sends it to the logs group."""
+    with open(LOG_FILE, "a") as log_file:
+        log_file.write(message + "\n")
+
+    if bot:
+        from config import LOGS_GROUP_ID
+        bot.send_message(chat_id=LOGS_GROUP_ID, text=message)
+
+def count_users(users):
+    """Returns the number of unique users using the bot."""
+    return len(users)
